48c92761b4fb522f89a1d5a535a3632026d1d791
raulgimenezdd <snip3raul@gmail.com>
2020-10-26 18:10:01 +0100

    calendar

--- ej-js/main.js
+++ ej-js/main.js
@@ -1,12 +1,12 @@
 function showElement(id) {
-  document.getElementById("main-block").style.display = "none";
-  document.getElementById("asignatura").style.display = "none";
-  document.getElementById("calificaciones").style.display = "none";
-  document.getElementById("contacto").style.display = "none";
-  document.getElementById("estudiantes").style.display = "none";
-  document.getElementById("faq").style.display = "none";
-  document.getElementById("foro").style.display = "none";
-  document.getElementById(id).style.display = "block";
+    document.getElementById("main-block").style.display = "none";
+    document.getElementById("asignatura").style.display = "none";
+    document.getElementById("calificaciones").style.display = "none";
+    document.getElementById("contacto").style.display = "none";
+    document.getElementById("estudiantes").style.display = "none";
+    document.getElementById("faq").style.display = "none";
+    document.getElementById("foro").style.display = "none";
+    document.getElementById(id).style.display = "block";
 }
 
 // Get the modal
@@ -19,57 +19,57 @@ var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function () {
-  modal.style.display = "block";
-  $(function () {
-    $(".calendar").simpleCalendar({
-      // displays events
-      displayEvent: true,
-      displayYear: true,
-      // event dates
-      events: [
-        // generate new event after tomorrow for one hour
-        {
-          startDate: new Date(new Date().setHours(new Date().getHours() + 24)).toDateString(),
-          endDate: new Date(new Date().setHours(new Date().getHours() + 25)).toISOString(),
-          summary: 'Visit of the Eiffel Tower'
-        },
-        // generate new event for yesterday at noon
-        {
-          startDate: new Date(new Date().setHours(new Date().getHours() - new Date().getHours() - 12, 0)).toISOString(),
-          endDate: new Date(new Date().setHours(new Date().getHours() - new Date().getHours() - 11)).getTime(),
-          summary: 'Restaurant'
-        },
-        // generate new event for the last two days
-        {
-          startDate: new Date(new Date().setHours(new Date().getHours() - 48)).toISOString(),
-          endDate: new Date(new Date().setHours(new Date().getHours() - 24)).getTime(),
-          summary: 'Visit of the Louvre'
-        }
-      ],
-      disableEventDetails: false,
-      disableEmptyDetails: false,
-      months: ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre'],
-      days: ['domingo', 'lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado'],
-      displayYear:true,
-      fixedStartDay:true
-  
-    });
-  });
-
+btn.onclick = function() {
+    modal.style.display = "block";
 }
 
+$(function() {
+    $(".calendar").simpleCalendar({
+        // displays events
+        displayEvent: true,
+        displayYear: true,
+        // event dates
+        events: [
+            // generate new event after tomorrow for one hour
+            {
+                startDate: new Date(new Date().setHours(new Date().getHours() + 24)).toDateString(),
+                endDate: new Date(new Date().setHours(new Date().getHours() + 25)).toISOString(),
+                summary: document.getElementById('ev1').innerText
+            },
+            // generate new event for yesterday at noon
+            {
+                startDate: new Date(new Date().setHours(new Date().getHours() - new Date().getHours() - 12, 0)).toISOString(),
+                endDate: new Date(new Date().setHours(new Date().getHours() - new Date().getHours() - 11)).getTime(),
+                summary: document.getElementById('ev2').innerText
+            },
+            // generate new event for the last two days
+            {
+                startDate: new Date(new Date().setHours(new Date().getHours() - 48)).toISOString(),
+                endDate: new Date(new Date().setHours(new Date().getHours() - 24)).getTime(),
+                summary: document.getElementById('ev3').innerText
+            }
+        ],
+        disableEventDetails: false,
+        disableEmptyDetails: false,
+        months: ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre'],
+        days: ['domingo', 'lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado'],
+        displayYear: true,
+        fixedStartDay: true,
+        onInit: function(calendar) {},
+        onMonthChange: function(month, year) {},
+        onDateSelect: function(date, events) {}
+    });
+});
 
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  modal.style.display = "none";
+span.onclick = function() {
+    modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
-
