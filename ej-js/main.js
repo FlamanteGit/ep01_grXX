4af459b19351bd8c101f89375eea9fbb1beb73c0
FlamanteGit <mohamedelaakil@gmail.com>
2020-10-26 17:21:42 +0100

    raul cabeza de huevo

--- ej-js/main.js
+++ ej-js/main.js
@@ -7,8 +7,69 @@ function showElement(id) {
   document.getElementById("faq").style.display = "none";
   document.getElementById("foro").style.display = "none";
   document.getElementById(id).style.display = "block";
-};
+}
 
-$(function () {
-  $("#container").simpleCalendar();
-});
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+  $(function () {
+    $(".calendar").simpleCalendar({
+      // displays events
+      displayEvent: true,
+      displayYear: true,
+      // event dates
+      events: [
+        // generate new event after tomorrow for one hour
+        {
+          startDate: new Date(new Date().setHours(new Date().getHours() + 24)).toDateString(),
+          endDate: new Date(new Date().setHours(new Date().getHours() + 25)).toISOString(),
+          summary: 'Visit of the Eiffel Tower'
+        },
+        // generate new event for yesterday at noon
+        {
+          startDate: new Date(new Date().setHours(new Date().getHours() - new Date().getHours() - 12, 0)).toISOString(),
+          endDate: new Date(new Date().setHours(new Date().getHours() - new Date().getHours() - 11)).getTime(),
+          summary: 'Restaurant'
+        },
+        // generate new event for the last two days
+        {
+          startDate: new Date(new Date().setHours(new Date().getHours() - 48)).toISOString(),
+          endDate: new Date(new Date().setHours(new Date().getHours() - 24)).getTime(),
+          summary: 'Visit of the Louvre'
+        }
+      ],
+      disableEventDetails: false,
+      disableEmptyDetails: false,
+      months: ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre'],
+      days: ['domingo', 'lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado'],
+      displayYear:true,
+      fixedStartDay:true
+  
+    });
+  });
+
+}
+
+
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
